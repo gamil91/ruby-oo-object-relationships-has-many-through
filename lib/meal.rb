88dd2d47dec287e7355395e58b12f37acc465944
gamil91 <alexis_gamil99@yahoo.com.com>
2020-10-12 21:35:11 -0700

    done

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
 
+@@all = []
+
+def self.all
+    @@all
+end
+
+attr_accessor :waiter, :customer, :total, :tip
+
+def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    self.class.all << self
+end
+
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Waiter
 
+@@all = []
+
+def self.all
+    @@all
 end
+
+attr_reader :name, :years
+
+def initialize(name, years_of_experience)
+    @name = name
+    @years = years_of_experience
+    self.class.all << self
+end
+
+def new_meal(customer, total, tip)
+    Meal.new(self, customer, total, tip)
+end
+
+def meals
+    Meal.all.select {|meal| meal.waiter == self}
+    
+end
+
+def best_tipper
+    self.meals.max_by {|meal| meal.tip}.customer
+end
+
+
+
+
+end
+
